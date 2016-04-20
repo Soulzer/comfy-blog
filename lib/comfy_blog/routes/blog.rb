3a72c74fe951c9917e7fbd0940445587ee5bb913
Soulzer <lokestella@gmail.com>
2016-04-20 22:55:42 +0800

    Update blog.rb

--- lib/comfy_blog/routes/blog.rb
+++ lib/comfy_blog/routes/blog.rb
@@ -5,6 +5,7 @@ class ActionDispatch::Routing::Mapper
     path = ['(:cms_path)', options[:path], '(:blog_path)'].join('/')
     
     scope :module => :comfy, :as => :comfy do
+      get 'indexall' => 'posts#indexall', :as => :posts_all
       namespace :blog, :path => path, :constraints => {:blog_path => /\w[a-z0-9_-]*/} do
         with_options :constraints => {:year => /\d{4}/, :month => /\d{1,2}/} do |o|
           o.get ':year'               => 'posts#index', :as => :posts_of_year
@@ -14,7 +15,7 @@ class ActionDispatch::Routing::Mapper
         post ':slug/comments' => 'comments#create', :as => :comments
         get  ':slug'          => 'posts#show',     :as => :post
         get  '/'              => 'posts#index',     :as => :posts
-        get 'indexall' => 'posts#indexall', :as => :posts_all
+        
       end
     end
   end
