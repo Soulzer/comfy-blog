561289148b4e3e07ddc95f0cd59caef28581a1fb
Soulzer <lokestella@gmail.com>
2016-04-20 22:07:56 +0800

    Update blog.rb

--- lib/comfy_blog/routes/blog.rb
+++ lib/comfy_blog/routes/blog.rb
@@ -14,7 +14,7 @@ class ActionDispatch::Routing::Mapper
         post ':slug/comments' => 'comments#create', :as => :comments
         get  ':slug'          => 'posts#serve',     :as => :post
         get  '/'              => 'posts#serve',     :as => :posts
-        get '/allposts' => 'posts#indexall'
+        get '/allposts' => 'posts#indexall', :as => :posts_all
       end
     end
   end
