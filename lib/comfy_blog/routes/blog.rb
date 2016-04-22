1c7d90387d97765e6a7b597af1ec4f8857ab6fd4
Soulzer <lokestella@gmail.com>
2016-04-22 13:10:39 +0800

    Update blog.rb

--- lib/comfy_blog/routes/blog.rb
+++ lib/comfy_blog/routes/blog.rb
@@ -18,6 +18,7 @@ class ActionDispatch::Routing::Mapper
         get  ':slug'          => 'posts#serve',     :as => :post
         get  '/'              => 'posts#serve',     :as => :posts
         get 'all_posts' => 'posts#serve', :as => :posts_all
+        get 'tags/:tag', to: 'posts#index', as: :tag
         
       end
     end
