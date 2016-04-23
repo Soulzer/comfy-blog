211c3ba7f2be81c257a7541e3d4a8437af251f1e
pierreyfl <pierreyam@icloud.com>
2016-04-24 00:25:36 +0800

    Update post.rb

--- app/models/comfy/blog/post.rb
+++ app/models/comfy/blog/post.rb
@@ -37,9 +37,7 @@ class Comfy::Blog::Post < ActiveRecord::Base
   before_validation :set_slug,
                     :set_published_at,
                     :set_date
-  def self.tagged_with(name)
-  Tag.find_by_name!(name).posts
-end
+
 protected
   
   def set_slug
